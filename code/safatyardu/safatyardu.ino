94ef2ff7324f6e0c6c094aff8a59bd805d5ec748
Samuel Costa Carvalho <sccarvalho1@latam.stefanini.com>
2018-11-01 18:02:51 -0300

    Atualização

--- code/safatyardu/safatyardu.ino
+++ code/safatyardu/safatyardu.ino
@@ -5,130 +5,142 @@ SoftwareSerial ESP(2, 3);
 
 // SENSOR DE MOVIMENTACAO E DEBUG
 #define PINMOV 7
-#define DEBUG false
+#define DEBUG true
 
-void setup(){
+void setup() {
 
-    // OPEN SERIAL COMMUNICATIONS AND WAIT FOR PORT TO OPEN ESP8266:
-    Serial.begin(9600);
+  // OPEN SERIAL COMMUNICATIONS AND WAIT FOR PORT TO OPEN ESP8266:
+  Serial.begin(9600);
 
-    // CONECNTANDO AO ESP
-    ESP.begin(9600);
-	
-	//DETERMINANDO ENTRADA DO PINO DO SENSOR DE MOVIMENTO
-    pinMode(PINMOV, INPUT);
+  // CONECNTANDO AO ESP
+  ESP.begin(9600);
 
-    // INICIALIZAR WIFI COM ESP
-    initwifi();
-	
+  //DETERMINANDO ENTRADA DO PINO DO SENSOR DE MOVIMENTO
+  pinMode(PINMOV, INPUT);
+
+  Serial.println("Iniciando Conexao Wifi ...");
+
+  // INICIALIZAR WIFI COM ESP
+  initwifi();
+
 }
 
 // FUNCTION LOOP
-void loop(){
-	
-	//VERIFICANDO DETECÇÃO DE MOVIMENTO
-    boolean GETMOV = digitalRead(PINMOV);
+void loop() {
 
-    delay(1000);
+  //VERIFICANDO DETECÇÃO DE MOVIMENTO
+  boolean GETMOV = digitalRead(PINMOV);
 
-    if (GETMOV){
+  delay(1000);
 
-        Serial.println("## DETECTION ##");
-		
-		// REQUEST DE MOVIMENTO
-		requestmov();
-		
-    }
+  if (GETMOV) {
+
+    Serial.println("## DETECTION ##");
+
+    // REQUEST DE MOVIMENTO
+    requestmov();
+
+  }
 
 }
 
 // REALIZANDO A CONEXAO COM O WIFI
-void initwifi(){
-    
-    // RST
-    sendwifi("AT+RST\r\n", 2000, DEBUG); 
-    
-    // CONECTA A REDE WIRELESS
-    sendwifi("AT+CWJAP=\"SSID\",\"SENHA\"\r\n", 2000, DEBUG);
-    
-    delay(3000);
+void initwifi() {
 
-    // MODO WIFI NÚMERO 1( ESTAÇÃO-CLIENTE )
-    sendwifi("AT+CWMODE=1\r\n", 1000, DEBUG);
+  // ALTERANDO TAXA DO ESP
+  sendwifi("AT+IPR=9600\r\n", 2000, DEBUG);
 
-    // MOSTRA O ENDERECO IP
-    sendwifi("AT+CIFSR\r\n", 1000, DEBUG);
+  // RST
+  sendwifi("AT+RST\r\n", 2000, DEBUG);
 
-    // CONFIGURA PARA MULTIPLAS CONEXOES ( MAX=4 )
-    sendwifi("AT+CIPMUX=1\r\n", 1000, DEBUG);
+  // CONECTA A REDE WIRELESS
+  sendwifi("AT+CWJAP=\"Home Net\",\"Brasil7x1\"\r\n", 2000, DEBUG);
+
+  delay(3000);
+
+  // MODO WIFI NÚMERO 1( ESTAÇÃO-CLIENTE )
+  sendwifi("AT+CWMODE=1\r\n", 1000, DEBUG);
+
+  // MOSTRA O ENDERECO IP
+  sendwifi("AT+CIFSR\r\n", 1000, DEBUG);
+
+  // CONFIGURA PARA MULTIPLAS CONEXOES ( MAX=4 )
+  sendwifi("AT+CIPMUX=1\r\n", 1000, DEBUG);
 
 }
 
 // FUNÇAO PARA ENVIAR DADOS PARA O ESP8266.
-String sendwifi(String command, long timeout, boolean debug){
+String sendwifi(String command, long timeout, boolean debug) {
 
-    String response = "";
+  // ESCREVENDO COMANDO
+  Serial.println(command);
 
-    // ESCREVENDO NO MODULO ESP8266
-    ESP.println(command); 
+  // ESCREVENDO NO MODULO ESP8266
+  ESP.println(command);
 
-    // CAPTURA MILISEGUNDOS
-    long times = millis();
-    
-    while ((times + timeout) > millis()){
-        while (ESP.available()){
+  // CAPTURA MILISEGUNDOS
+  long times = millis();
 
-            // O ESP TEM DADOS, ASSIM MOSTRA A SUA SAÍDA PARA A JANELA SERIAL
-            char c = ESP.read(); 
-            response += c;
+  // STRING DE RETORNO
+  String response = "";
 
-        }
+  while ((times + timeout) > millis()) {
+    while (ESP.available()) {
+
+      // O ESP TEM DADOS, ASSIM MOSTRA A SUA SAÍDA PARA A JANELA SERIAL
+      char c = ESP.read();
+      response += c;
+
     }
+  }
 
-    // ESCREVENDO DEBUG NO TERMINAL
-    if (debug){
-        Serial.println(response);
-    }
+  // ESCREVENDO DEBUG NO TERMINAL
+  if (debug) {
+    Serial.println(response);
+  }
 
-    return response;
+  return response;
 
 }
 
 // REALIZANDO REQUISICAO DA DETECCAO DE MOVIMENTO
-void requestmov(){
+void requestmov() {
 
-    Serial.println("# INIT REQUEST");
+  Serial.println("# INIT REQUEST");
 
-    // ASSEGURA QUE A CONEXÃO 0 ESTÁ LIVRE PRA SER USADA
-    sendwifi("AT+CIPCLOSE=0\r\n", 2000, DEBUG);
+  // ASSEGURA QUE A CONEXÃO 0 ESTÁ LIVRE PRA SER USADA
+  sendwifi("AT+CIPCLOSE=0\r\n", 2000, DEBUG);
 
-    sendwifi("AT+CIPSTART=0,\"TCP\",\"safetyflask.herokuapp.com\",80\r\n", 1000, DEBUG);
-    
-    delay(1000);
+  sendwifi("AT+CIPSTART=0,\"TCP\",\"safetyflask.herokuapp.com\",80\r\n", 2000, DEBUG);
 
-    String request  = "";
-    String body     = "arduino=DFEFEGNE24324";
-    String command  = "";
+  delay(1000);
 
-    request += "POST /logs HTTP/1.1\r\n"; 
-    request += "Host: safetyflask.herokuapp.com\r\n";
-    request += "Authorization: Basic YWRtaW46c2VjcmV0\r\n";
-    request += "Content-Type: application/x-www-form-urlencoded\r\n";    
-    request += "Content-Length: "+body.length()+"\r\n";
-    request += body+"\r\n";
+  String request  = "";
+  String body     = "arduino=DFEFEGNE24324";
+  String command  = "";
 
-    command = "AT+CIPSEND=0,";
-    command += request.length();
-    command += "\r\n";
+  request += "POST /logs HTTP/1.1\r\n";
+  request += "Host: safetyflask.herokuapp.com\r\n";
+  request += "Authorization: Basic YWRtaW46c2VjcmV0\r\n";
+  request += "Content-Type: application/x-www-form-urlencoded\r\n";
+  request += "cache-control: no-cache\r\n";
+  request += "Content-Length: ";
+  request += body.length();
+  request += "\r\n";
+  request += body + "\r\n";
 
-    sendwifi(command, 1000, DEBUG);
+  command = "AT+CIPSEND=0,";
+  command += request.length();
+  command += "\r\n";
 
-    sendwifi(request, 1000, DEBUG);
+  sendwifi(command, 1000, DEBUG);
 
-    delay(5000);
+  sendwifi(request, 1000, DEBUG);
 
-    sendwifi("AT+CIPCLOSE=0\r\n", 1000, DEBUG);
+  delay(5000);
 
-    Serial.println("# END REQUEST");
+  sendwifi("AT+CIPCLOSE=0\r\n", 1000, DEBUG);
+
+  Serial.println("# END REQUEST");
 
 }
